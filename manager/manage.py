843e0b134aea76357588fc824308549247fdd568
Nicolas Pieuchot <nls.pct@gmail.com>
2013-11-13 21:51:00 +0100

    Changing directory structure.

--- manager/manage.py
+++ manager/manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "manager.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pod.manager.settings")
 
     from django.core.management import execute_from_command_line
 
--- manager/pod/manager/settings.py
+++ manager/pod/manager/settings.py
@@ -1,5 +1,5 @@
 """
-Django settings for manager project.
+Django settings for pod.manager project.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/1.6/topics/settings/
@@ -36,6 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'pod.manager.apps.brand'
 )
 
 MIDDLEWARE_CLASSES = (
@@ -47,9 +48,9 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
-ROOT_URLCONF = 'manager.urls'
+ROOT_URLCONF = 'pod.manager.urls'
 
-WSGI_APPLICATION = 'manager.wsgi.application'
+WSGI_APPLICATION = 'pod.manager.wsgi.application'
 
 
 # Database
