a1b7aae5db3532800a01cbd538584962c7389487
Nicolas Pieuchot <nls.pct@gmail.com>
2013-11-18 16:03:28 +0100

    Removing useless settings

--- manager/wsgi.py
+++ manager/wsgi.py
@@ -7,9 +7,6 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
-import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "manager.settings.dev")
-
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
 application = Cling(get_wsgi_application())
