4e613eb798395c84150c3e0bd0398cb6273680f4
Nicolas Pieuchot <nls.pct@gmail.com>
2013-11-19 12:29:18 +0100

    Conforming as much as possible to PEP8 recommandations

--- manager/apps/brand/models.py
+++ manager/apps/brand/models.py
@@ -14,16 +14,16 @@ from django.db import models
 class Brand(models.Model):
     bsin = models.CharField(db_column='BSIN', primary_key=True, max_length=6)
     brand_nm = models.CharField(db_column='BRAND_NM', max_length=255)
-    owner_cd = models.ForeignKey('BrandOwner', db_column='OWNER_CD',
-        blank=True, null=True)
+    owner_cd = models.ForeignKey(
+        'BrandOwner', db_column='OWNER_CD', blank=True, null=True)
     brand_type_cd = models.ForeignKey('BrandType', db_column='BRAND_TYPE_CD')
-    brand_link = models.CharField(db_column='BRAND_LINK', max_length=255,
-        blank=True, null=True)
+    brand_link = models.CharField(
+        db_column='BRAND_LINK', max_length=255, blank=True, null=True)
     flag_delete = models.BooleanField(db_column='FLAG_DELETE', default=False)
-    last_modified = models.DateTimeField(db_column='LAST_MODIFIED',
-        auto_now=True)
-    comments = models.CharField(db_column='COMMENTS', max_length=255,
-        blank=True, null=True)
+    last_modified = models.DateTimeField(
+        db_column='LAST_MODIFIED', auto_now=True)
+    comments = models.CharField(
+        db_column='COMMENTS', max_length=255, blank=True, null=True)
 
     class Meta:
         db_table = 'brand'
@@ -46,8 +46,8 @@ class BrandOwner(models.Model):
 
 
 class BrandType(models.Model):
-    brand_type_cd = models.IntegerField(db_column='BRAND_TYPE_CD',
-        primary_key=True)
+    brand_type_cd = models.IntegerField(
+        db_column='BRAND_TYPE_CD', primary_key=True)
     brand_type_nm = models.CharField(db_column='BRAND_TYPE_NM', max_length=255)
 
     class Meta:
