ff29b9e3163a60ea4efc1f9607ff8aa2d83a5a5e
François Dupayrat <fradow@gmail.com>
2013-11-23 17:42:18 +0100

    Add Brands list view

---
+++ manager/apps/brand/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+from django.views.generic import View
+from .models import Brand, BrandOwner, BrandType
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
+
+class BrandView(View):
+    r"""
+    """
+
+    template_name = 'main/brandlist.jade'
+    allBrands = Brand.objects.all()
+
+    def get(self, request):
+        brand_list = Brand.objects.all()
+        paginator = Paginator(brand_list, 25) # Show 100 brands per page
+
+        page = request.GET.get('page')
+        try:
+            brands = paginator.page(page)
+        except PageNotAnInteger:
+            # If page is not an integer, deliver first page.
+            brands = paginator.page(1)
+        except EmptyPage:
+            # If page is out of range (e.g. 9999), deliver last page of results.
+            brands = paginator.page(paginator.num_pages)
+        return render(request, self.template_name, {'brands': brands})
