f324b025085ce9e6537bfab5a7a7dffe7fb6eae4
François Dupayrat <fradow@gmail.com>
2013-11-23 21:00:49 +0100

    Refactor BrandView url regexp

--- manager/apps/brand/urls.py
+++ manager/apps/brand/urls.py
@@ -6,5 +6,5 @@ urlpatterns = patterns(
     '',
     url(r'^$', BrandListView.as_view(), name='brandlist'),
     #regular expression for a BSIN
-    url(r'^[1-9A-NP-Z]{6,6}', BrandView.as_view(), name='brand'),
+    url(r'^[1-9A-NP-Z]{6}', BrandView.as_view(), name='brand'),
 )
