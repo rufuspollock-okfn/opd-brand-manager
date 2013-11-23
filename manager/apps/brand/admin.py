a2caf93c6c0885e3e808f573a8081c93860e9e4d
Nicolas Pieuchot <nls.pct@gmail.com>
2013-11-23 23:56:07 +0100

    New administration filter and search fields for the models

--- manager/apps/brand/admin.py
+++ manager/apps/brand/admin.py
@@ -2,8 +2,27 @@ from django.contrib import admin
 from django import forms
 from .models import Brand, BrandOwner, BrandType
 
-admin.site.register(BrandOwner)
-admin.site.register(BrandType)
+
+class BrandTypeAdmin(admin.ModelAdmin):
+    actions = None
+
+    # Never delete a brand, update its BSIN
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+admin.site.register(BrandType, BrandTypeAdmin)
+
+
+class BrandOwnerAdmin(admin.ModelAdmin):
+    actions = None
+    list_display = ('owner_nm', 'owner_link')
+    search_fields = ['owner_nm']
+
+    # Never delete a brand, update its BSIN
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+admin.site.register(BrandOwner, BrandOwnerAdmin)
 
 
 class BrandAdmin(admin.ModelAdmin):
@@ -11,8 +30,10 @@ class BrandAdmin(admin.ModelAdmin):
     list_display = ('bsin', 'brand_nm', 'brand_logo_admin', 'flag_delete')
     fields = (
         'bsin', 'brand_nm', 'owner_cd', 'brand_type_cd', 'brand_link',
-        ('flag_delete', 'comments'), 'last_modified')
+        'brand_logo', ('flag_delete', 'comments'), 'last_modified')
     readonly_fields = ('bsin', 'last_modified')
+    search_fields = ['bsin', 'brand_nm', 'owner_cd__owner_nm']
+    list_filter = ('flag_delete', )
 
     # Never delete a brand, update its BSIN
     def has_delete_permission(self, request, obj=None):
