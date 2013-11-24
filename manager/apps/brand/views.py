2f0a3fe18e90b01123f30a79f6c3d25bd8c13310
François Dupayrat <fradow@gmail.com>
2013-11-24 09:14:33 +0100

    Remove unused import

--- manager/apps/brand/views.py
+++ manager/apps/brand/views.py
@@ -4,7 +4,6 @@ from .models import Brand, BrandOwner, BrandType
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.core.exceptions import ObjectDoesNotExist
 from django.http import HttpResponseNotFound
-import os
 
 
 class BrandListView(View):
