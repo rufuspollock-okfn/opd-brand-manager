91751207e0426b3c8a7a32f5f2ed0baa786d7d02
François Dupayrat <fradow@gmail.com>
2013-11-27 21:50:25 +0100

    Add owner list

--- manager/apps/brand/urls.py
+++ manager/apps/brand/urls.py
@@ -5,6 +5,7 @@ from manager.apps.brand.views import BrandView
 urlpatterns = patterns(
     '',
     url(r'^$', BrandListView.as_view(), name='brandlist'),
+    url(r'^owner', OwnerListView.as_view(), name='ownerlist'),
     #regular expression for a BSIN
     url(r'^(?P<bsin>[1-9A-NP-Z]{6})', BrandView.as_view(), name='brand'),
 )
