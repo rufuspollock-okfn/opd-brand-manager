fccd5c252ba3906653f6bab20633dceef5d64806
Feng-Gao <gaofeng860918@gmail.com>
2013-11-29 18:49:34 +0800

    [admin,okfn/brand-manager#27] create a new widget to show the thumbnail in admin 'change' form

---
+++ manager/apps/brand/widget.py
@@ -0,0 +1,19 @@
+from django import forms
+from django.utils.safestring import mark_safe
+
+class AdminImageWidget(forms.FileInput):
+    """
+    A ImageField Widget for admin that shows a thumbnail.
+    """
+
+    def __init__(self, attrs={}):
+        super(AdminImageWidget, self).__init__(attrs)
+
+    def render(self, name, value, attrs=None):
+        output = []
+        if value and hasattr(value, "url"):
+            output.append(('<a target="_blank" href="%s">'
+                           '<img width="128" height"128" src="%s"/></a> '
+                           % (value.url, value.url)))
+        output.append(super(AdminImageWidget, self).render(name, value, attrs))
+        return mark_safe(u''.join(output))
