7290b589219587e161380da2f323c581c2b436eb
François Dupayrat <fradow@gmail.com>
2013-11-30 18:24:12 +0100

    Fix BrandProposalForm to use BrandType in a choice

--- manager/apps/brand/forms.py
+++ manager/apps/brand/forms.py
@@ -1,9 +1,10 @@
 from django import forms
+from .models import BrandType
 
 
 class BrandProposalForm(forms.Form):
     brand_nm = forms.CharField(max_length=255, label='Brand name')
-    brand_type = forms.ChoiceField(choices=(('test', '1'), ('test2', '2')))
+    brand_type = forms.ModelChoiceField(queryset=BrandType.objects.all())
     owner_nm = forms.CharField(
         max_length=255, label='Owner name', required=False)
     brand_link = forms.URLField(
