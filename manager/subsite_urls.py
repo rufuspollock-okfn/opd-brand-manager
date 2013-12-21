39a460b521a1b02dfa9b564f630de5bd07acc31b
Nicolas Pieuchot <nls@pct.fr.eu.org>
2013-12-22 00:42:54 +0100

    Adding MEDIA_ROOT url for testing with local media files. Think to also uncomment the local MEDIA_URL in the dev.py MEDIA_URL if testing with local media files.

--- manager/subsite_urls.py
+++ manager/subsite_urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from tastypie.api import Api
 from manager.apps.brand.api import BrandResource  # [#58] , BrandOwnerResource
+from django.conf import settings
 
 admin.autodiscover()
 
@@ -22,3 +23,9 @@ urlpatterns = patterns(
 
     url(r'^admin/', include(admin.site.urls)),
 )
+
+if settings.DEBUG:
+    # static files (images, css, javascript, etc.)
+    urlpatterns += patterns('', (
+        r'^media/(?P<path>.*)$', 'django.views.static.serve',
+        {'document_root': settings.MEDIA_ROOT}))
