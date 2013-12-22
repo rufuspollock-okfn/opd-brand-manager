6c60e3c0b52c92b0893743c99a8f33e8e9e86945
Nicolas Pieuchot <nls@pct.fr.eu.org>
2013-12-22 13:53:17 +0100

    Update logging for Sentry.

--- manager/settings/prod.py
+++ manager/settings/prod.py
@@ -136,3 +136,51 @@ RECAPTCHA_USE_SSL = True
 # framework
 
 SITE_ID = 1
+
+# Logging configuration
+# http://raven.readthedocs.org/en/latest/config/django.html#integration-with-
+# logging
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'root': {
+        'level': 'WARNING',
+        'handlers': ['sentry'],
+    },
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s '
+                      '%(process)d %(thread)d %(message)s'
+        },
+    },
+    'handlers': {
+        'sentry': {
+            'level': 'ERROR',
+            'class':
+            'raven.contrib.django.raven_compat.handlers.SentryHandler',
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        }
+    },
+    'loggers': {
+        'django.db.backends': {
+            'level': 'ERROR',
+            'handlers': ['console'],
+            'propagate': False,
+        },
+        'raven': {
+            'level': 'DEBUG',
+            'handlers': ['console'],
+            'propagate': False,
+        },
+        'sentry.errors': {
+            'level': 'DEBUG',
+            'handlers': ['console'],
+            'propagate': False,
+        },
+    },
+}
